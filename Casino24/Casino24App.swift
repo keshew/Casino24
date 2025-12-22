fe0032d85c79b127a9da5d056cced7a55dba9375
keshew <artyom.korotckov2017@yandex.ru>
2025-12-22 15:00:31 +0500

    Initial Commit

---
+++ Casino24/Casino24App.swift
@@ -0,0 +1,17 @@
+//
+//  Casino24App.swift
+//  Casino24
+//
+//  Created by Артём Коротков on 22.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Casino24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Casino24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Casino24
+//
+//  Created by Артём Коротков on 22.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
